6d2a2d265c13559e24d4b53af042a30b451ec369
aqd14 <aqd14@msstate.edu>
2017-03-29 13:00:08 -0500

    Fix bug wrong file path for chrome driver

--- src/org/main/java/common/CommonDefine.java
+++ src/org/main/java/common/CommonDefine.java
@@ -8,7 +8,7 @@ package org.main.java.common;
  *
  */
 public class CommonDefine {
-	public static final String WINDOWS_WEB_DRIVER_PATH = "webdriver/Linux/chromedriver";
+	public static final String WINDOWS_WEB_DRIVER_PATH = "webdriver/Windows/chromedriver.exe";
 	public static final String MACOS_WEB_DRIVER_PATH = "webdriver/MacOS/chromedriver";
-	public static final String LINUX_WEB_DRIVER_PATH = "webdriver/Linux/chromedriver.exe";
+	public static final String LINUX_WEB_DRIVER_PATH = "webdriver/Linux/chromedriver";
 }
